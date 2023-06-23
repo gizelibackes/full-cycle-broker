43a70ab88da2b61a17fb18581a34b5ea73541976
Gizeli Backes <116848131+gizelibackes@users.noreply.github.com>
2023-06-23 17:31:51 -0300

    aula 2 - Comunicação com Kafka

---
+++ internal/market/entity/book.go
@@ -0,0 +1,94 @@
+package entity
+
+import (
+	"container/heap"
+	"sync"
+)
+
+type Book struct {
+	Order         []*Order
+	Transaction   []*Transaction
+	OrdersChan    chan *Order // input - recebe order de compra e venda do kafka
+	OrdersChanOut chan *Order
+	Wg            *sync.WaitGroup
+}
+
+func NewBook(orderChan chan *Order, orderChanOut chan *Order, wg *sync.WaitGroup) *Book {
+	return &Book{
+		Order:         []*Order{},
+		Transaction:   []*Transaction{},
+		OrdersChan:    orderChan,
+		OrdersChanOut: orderChanOut,
+		Wg:            wg,
+	}
+}
+
+func (b *Book) Trade() {
+
+	heap.Init(buyOrders)
+	heap.Init(sellOrders)
+
+	for order := range b.OrdersChan {
+		if order.OrderType == "BUY" {
+			buyOrders.Push(order)
+			if sellOrders.Len() > 0 && sellOrders.Orders[0].Price <= order.Price {
+				sellOrder := sellOrders.Pop().(*Order)
+				if sellOrder.PendingShares > 0 {
+					transaction := NewTransaction(sellOrder, order, order.Shares, sellOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					sellOrder.Transactions = append(sellOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					b.OrdersChanOut <- sellOrder
+					b.OrdersChanOut <- order
+					if sellOrder.PendingShares > 0 {
+						sellOrders.Push(sellOrder)
+					}
+				}
+			}
+		} else if order.OrderType == "SELL" {
+			sellOrders.Push(order)
+			if buyOrders.Len() > 0 && buyOrders.Orders[0].Price >= order.Price {
+				buyorder := buyOrders.Pop().(*Order)
+				if buyorder.PendingShares > 0 {
+					transaction := NewTransaction(order, buyOrder, order.Shares, buyOrder.Price)
+					b.AddTransaction(transaction, b.Wg)
+					buyOrder.Transactions = append(buyOrder.Transactions, transaction)
+					order.Transactions = append(order.Transactions, transaction)
+					b.OrdersChanOut <- buyorder
+					b.OrdersChanOut <- order
+					if buyOrder.PendingShares > 0 {
+						buyOrders.Push(buyorder)
+					}
+
+				}
+			}
+		}
+	}
+}
+
+func (b *Book) AddTransaction(transaction *Transaction, wg *sync.WaitGroup) {
+	defer wg.Done() // defer seta o ultimo comando a ser executado no bloco
+
+	sellingShares := transaction.SellingOrder.PendingShares
+	buyingShares := transaction.BuyingOrder.PendingShares
+
+	minShares := sellingShares
+	if buyingShares < minShares {
+		minShares = buyingShares
+	}
+
+	transaction.SellingOrder.Investor.UpdateAssetPosition(transaction.SellingOrder.Asset.ID, -minShares)
+	transaction.SellingOrder.PendingShares -= minShares
+	transaction.BuyingOrder.Investor.UpdateAssetPosition(transaction.BuyingOrder.Asset.ID, minShares)
+	transaction.BuyingOrder.PendingShares -= minShares
+
+	transaction.Total = float64(transaction.Shares) * transaction.BuyingOrder.Price
+
+	if transaction.BuyingOrder.PendingShares == 0 {
+		transaction.BuyingOrder.Status = "CLOSED"
+	}
+	if transaction.SellingOrder.PendingShares == 0 {
+		transaction.SellingOrder.Status = "CLOSED"
+	}
+	b.Transaction = append(b.Transaction, transaction)
+}
---
+++ internal/market/entity/investor.go
@@ -0,0 +1,54 @@
+// CRIAÇAO DO ARQUVIO DO INVESTIDOR
+// CODIGO RESPONSAVEL PELA CRIACAO DO INVESTIDOR E PELA POSICAO
+// OU SEJA, PELA CARTEIRA QUE ESSE INVESTIDOR TEM DAS POSICOES DAS ACOES DELE
+
+package entity
+
+type Investor struct {
+	ID            string
+	Name          string
+	AssetPosition []*InvestorAssetPosition
+}
+
+func NewInvestor(id string) *Investor {
+	return &Investor{
+		ID:            id,
+		AssetPosition: []*InvestorAssetPosition{},
+	}
+}
+
+func (i *Investor) InvestorAssetPosition(assetPosition *InvestorAssetPosition) {
+	i.AssetPosition = append(i.AssetPosition, assetPosition)
+}
+
+func (i *Investor) UpdateAssetPosition(assetID string, qtdShares int) {
+	assetPosition := i.GetAssetPosition(assetID)
+	if assetPosition == nil {
+		i.AssetPosition = append(i.AssetPosition, NewInvestorAssetPositon(assetID, qtdShares))
+
+	} else {
+		assetPosition.Shares += qtdShares
+	}
+
+}
+
+func (i *Investor) GetAssetPosition(assetID string) *InvestorAssetPosition {
+	for _, assetPosition := range i.AssetPosition {
+		if assetPosition.AssetID == assetID {
+			return assetPosition
+		}
+	}
+	return nil
+}
+
+type InvestorAssetPosition struct {
+	AssetID string
+	Shares  int
+}
+
+func NewInvestorAssetPositon(assetID string, shares int) *InvestorAssetPosition {
+	return &InvestorAssetPosition{
+		AssetID: assetID,
+		Shares:  shares,
+	}
+}
